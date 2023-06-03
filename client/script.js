8a6607de678c8225b45dc13c9052fba6eb61a65c
unknown <silvesda@gmail.com>
2023-06-03 11:14:20 +0200

    first contributions sir

--- client/script.js
+++ client/script.js
@@ -1,18 +1,6 @@
-const socket = new WebSocket("ws://localhost:3000");
-
-socket.addEventListener("open", (event) => {
-  console.log("WebSocket connected!");
-  socket.send("Hello, server!");
-});
-
-socket.addEventListener("message", (event) => {
-  console.log(`Received message: ${event.data}`);
-});
-
-socket.addEventListener("close", (event) => {
-  console.log("WebSocket closed.");
-});
-
-socket.addEventListener("error", (event) => {
-  console.error("WebSocket error:", event);
+document.getElementById('send1').addEventListener('click', function() {
+  var message = document.getElementById('message1').value;
+  var messages = document.getElementById('chatMessages');
+  messages.innerHTML += '<div class="message1"><img src="img/profile.jpg" alt="User 1 Profile Picture"><p>' + message + '</p></div>';
+  document.getElementById('message1').value = '';
 });
