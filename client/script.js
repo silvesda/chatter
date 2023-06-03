03c4bb2599e5a096da70f5ca38029bfaa2ecf739
unknown <silvesda@gmail.com>
2023-06-03 11:57:39 +0200

    first style corrections

--- client/script.js
+++ client/script.js
@@ -1,6 +1,6 @@
 document.getElementById('send1').addEventListener('click', function() {
   var message = document.getElementById('message1').value;
   var messages = document.getElementById('chatMessages');
-  messages.innerHTML += '<div class="message1"><img src="img/profile.jpg" alt="User 1 Profile Picture"><p>' + message + '</p></div>';
+  messages.innerHTML += '<div class="message1"><img src="img/profile.png" alt=":D"><p>' + message + '</p></div>';
   document.getElementById('message1').value = '';
 });
